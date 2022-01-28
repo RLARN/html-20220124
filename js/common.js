acaf5167bd4b72650c5ff3d1a6f3ab7fb73b7bc8
RLARN <msjdoc0645@naver.com>
2022-01-28 22:26:39 +0900

    220128

--- js/common.js
+++ js/common.js
@@ -18,6 +18,65 @@ $(document).ready(function(){
     $(this).find('.submenuArea').slideToggle('fast');
   });
 
+  // site Map Close
+  $('.sitemapOpen').click(function(){
+    $('.siteMap').fadeIn('fast');
+  });
+
+    $('.siteMap .close').click(function(){
+      $('.siteMap').fadeOut('fast');
+  });
+
+  $('.siteMap .bg').click(function(){
+    $('.siteMap').fadeOut('fast');
+  });
+
+  //사업안내 탭
+  $('.section01 .area button').click(function(){
+    $('.section01 .area button').removeClass('on');
+    $('.section01 .right').css('on');
+    $(this).addClass('on');
+
+    var subid = $(this).attr('data-tab');
+    $('#'+subid).addClass('on');
+
+  });
+
+
+  //next
+  $('#sub01 .next').click(function(){
+    $('.section01 .area button:nth-child(2)').click();
+  });
+  $('#sub02 .next').click(function(){
+    $('.section01 .area button:nth-child(3)').click();
+  });
+  $('#sub03 .next').click(function(){
+    $('.section01 .area button:nth-child(4)').click();
+  });
+  $('#sub04 .next').click(function(){
+    $('.section01 .area button:nth-child(5)').click();
+  });
+  $('#sub05 .next').click(function(){
+    $('.section01 .area button:nth-child(1)').click();
+  });
+
+  //Prev
+  $('#sub01 .prev').click(function(){
+    $('.section01 .area button:nth-child(5)').click();
+  });
+  $('#sub02 .prev').click(function(){
+    $('.section01 .area button:nth-child(1)').click();
+  });
+  $('#sub03 .prev').click(function(){
+    $('.section01 .area button:nth-child(2)').click();
+  });
+  $('#sub04 .prev').click(function(){
+    $('.section01 .area button:nth-child(3)').click();
+  });
+  $('#sub05 .prev').click(function(){
+    $('.section01 .area button:nth-child(4)').click();
+  });
+
   //notice tab
   $('.section02 .tabArea button').click(function(){
     $('.section02 .tabArea button').removeClass('on');
@@ -51,4 +110,19 @@ $(document).ready(function(){
         $(this).toggleClass('on');
       });
 
+      //top button
+      $(window).scroll(function(){
+        if($(document).scrollTop() > 0){
+          $('.topBtn').fadeIn();
+        }else{
+          $('.topBtn').fadeOut();
+        }
+      });
+
+      $('.topBtn').click(function(){
+        $('html, body') .animate({
+          scrollTop : 0
+        },400);
+        return false;
+      });
 });
